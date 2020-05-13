5b4afdeaf2a2820ff2aca6eb5fa979f1aa0b5903
Agus <a.martinez.p97@gmail.com>
2020-05-13 18:38:32 +0200

    Esquelet del projecte

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+import pytest
+from src.main.Viatge import Viatge
+
---
+++ src/main/Viatge.py
@@ -0,0 +1,6 @@
+from src.main.Hotel import Hotel
+from src.main.Cotxe import Cotxe
+from src.main.Vol import Vol
+
+class Viatge:
+
